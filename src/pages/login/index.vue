96f70b914d200c2a1e38f5c3b78befce144c8899
Pedro Martins <rechousa@gmail.com>
2019-11-19 00:26:25 +0000

    Added Form validation with vuelidate

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -6,4 +6,4 @@
 export default {
   middleware: "auth"
 };
-</script>
+</script> 
--- src/pages/register/index.vue
+++ src/pages/register/index.vue
@@ -6,9 +6,9 @@
       </md-card-header>
 
       <!-- Register Form -->
-      <form @submit.prevent="registerUser">
+      <form @submit.prevent="validateForm">
         <md-card-content>
-          <md-field md-clearable>
+          <md-field md-clearable :class="getValidationClass('email')">
             <label for="email">Email</label>
             <md-input
               :disabled="loading"
@@ -18,9 +18,11 @@
               autocomplete="email"
               v-model="form.email"
             />
+            <span class="md-error" v-if="!$v.form.email.required">The email is required</span>
+            <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
           </md-field>
 
-          <md-field md-clearable>
+          <md-field :class="getValidationClass('password')">
             <label for="password">Password</label>
             <md-input
               :disabled="loading"
@@ -30,6 +32,9 @@
               autocomplete="password"
               v-model="form.password"
             />
+            <span class="md-error" v-if="!$v.form.password.required">The password is required</span>
+            <span class="md-error" v-else-if="!$v.form.password.minLength">Password too short</span>
+            <span class="md-error" v-else-if="!$v.form.password.maxLength">Password too long</span>
           </md-field>
         </md-card-content>
 
@@ -45,14 +50,36 @@
 </template>
 
 <script>
+import { validationMixin } from "vuelidate";
+import {
+  required,
+  email,
+  minLength,
+  maxLength
+} from "vuelidate/lib/validators";
+
 export default {
   middleware: "auth",
+  mixins: [validationMixin],
   data: () => ({
     form: {
       email: "",
       password: ""
     }
   }),
+  validations: {
+    form: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(6),
+        maxLength: maxLength(20)
+      }
+    }
+  },
   computed: {
     loading() {
       return this.$store.getters.loading;
@@ -69,12 +96,26 @@ export default {
     }
   },
   methods: {
+    validateForm() {
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        this.registerUser();
+      }
+    },
     async registerUser() {
       await this.$store.dispatch("authenticateUser", {
         email: this.form.email,
         password: this.form.password,
         returnSecureToken: true
       });
+    },
+    getValidationClass(fieldName) {
+        const field = this.$v.form[fieldName];
+        if(field) {
+            return {
+                "md-invalid": field.$invalid && field.$dirty
+            }
+        } 
     }
   }
 };
